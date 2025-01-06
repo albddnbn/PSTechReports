b7cba6b31153bb753efe73b79322a98d4dbfa5f8
Alex B. <105134673+albddnbn@users.noreply.github.com>
2025-01-05 19:30:51 -0500

    Added Copy-RemoteFiles and Send-Files, passed initial testing
    Added Copy-RemoteFiles and Send-Files, passed initial testing

--- PSTechReports.psm1
+++ PSTechReports.psm1
@@ -1721,4 +1721,190 @@ function Get-TempProfiles {
     return $results
 }
 
+function Copy-RemoteFiles {
+    <#
+    .SYNOPSIS
+        Recursively grabs target files or folders from remote computer(s) and copies them to specified directory on local computer.
+
+    .DESCRIPTION
+        TargetPath specifies the target file(s) or folder(s) to target on remote machines.
+        TargetPath can be supplied as a single absolute path, comma-separated list, or array.
+        OutputPath specifies the directory to store the retrieved files.
+        Creates a subfolder for each target computer to store it's retrieved files.
+
+    .PARAMETER ComputerName
+        Target computer or computers of the function.
+        Single hostname, ex: 't-client-01' or 't-client-01.domain.edu'
+        Path to text file containing one hostname per line, ex: 'D:\computers.txt'
+        First section of a hostname to generate a list, ex: t-pc-0 will create a list of all hostnames that start with t-pc-0. (Possibly t-pc-01, t-pc-02, t-pc-03, etc.)
+
+    .PARAMETER OutputFolder
+        Path to folder to store retrieved files. Ex: 'C:\users\abuddenb\Desktop\grabbed-files'
+
+    .PARAMETER TargetPath
+        Path to file(s)/folder(s) to be grabbed from remote machines. Ex: 'C:\users\abuddenb\Desktop\test.txt'
+
+    .PARAMETER SendPings
+        'true' will use the switch parameter, 'false' will not. Kind of acts like a boolean.
+
+    .EXAMPLE
+        Copy-RemoteFiles -TargetPath "Users\Public\Desktop" -OutputPath "C:\Users\Public\Desktop" -TargetComputer "t-client-"
+    
+    .EXAMPLE
+        Copy-RemoteFiles -ComputerName test-client -TargetPath C:\test.txt -OutputFolder C:\testgrab2 -SendPings
+
+    #>
+    param(        
+        [Parameter(
+            Mandatory = $true
+        )]
+        $ComputerName,
+        [string]$TargetPath,
+        [string]$OutputFolder,
+        [switch]$SendPings
+    )
+
+    $ComputerName = GetTargets -TargetComputer $ComputerName
+
+    if ($SendPings) {
+        $ComputerName = TestConnectivity -ComputerName $ComputerName
+    }
+
+    if (-not(Test-Path "$OutputFolder" -erroraction SilentlyContinue)) {
+        New-Item -ItemType Directory -Path "$OutputFolder" -ErrorAction SilentlyContinue | out-null
+    }
+
+    ForEach ($single_computer in $ComputerName) {
+        if ($single_computer) {
+            $target_network_path = $targetpath -replace 'C:', "\\$single_computer\c$"
+            write-host "Checking $single_computer...$target_network_path"
+
+                if (Test-Path "$target_network_path" -erroraction SilentlyContinue) {
+
+                    
+                    # $target_session = New-PSSession $single_computer
+
+                    $target_filename = $targetpath | split-path -leaf
+
+
+                    Copy-Item -Path "$target_network_path" -Destination "$OutputFolder\$single_computer-$target_filename"  -Recurse
+                    Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Transfer of $targetpath ($single_computer) to $OutputFolder\$single_computer-$target_filename  complete." -foregroundcolor green
+                    
+                    # Remove-PSSession $target_session
+
+                }
+                else {
+                    Write-Host "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Failed to copy $targetpath on $single_computer to $OutputFolder on local computer (remote item may not exist)." -foregroundcolor red
+                }
+        }
+    }
+    ## Open output folder, pause.
+    if (Test-Path "$OutputFolder" -erroraction SilentlyContinue) {
+        Invoke-item "$OutputFolder"
+    }
+}
+
+function Send-Files {
+    <#
+    .SYNOPSIS
+        Sends a target file/folder from local computer to target path on remote computers.
+
+    .DESCRIPTION
+        You can enter both paths as if they're on local filesystem, the script should cut out any drive letters and insert the \\hostname\c$ for UNC path. The script only works for C drive on target computers right now.
+
+    .PARAMETER SourcePath
+        The path of the file/folder you want to send to target computers. 
+        ex: C:\users\public\desktop\test.txt, 
+        ex: \\networkshare\folder\test.txt
+
+    .PARAMETER DestinationPath
+        The path on the target computer where you want to send the file/folder. 
+        The script will cut off any preceding drive letters and insert \\hostname\c$ - so destination paths should be on C drive of target computers.
+        ex: C:\users\public\desktop\
+        The path should be a destination folder, script should create directories/subdirectories as required.
+
+    .PARAMETER ComputerName
+        Target computer or computers of the function.
+        Single hostname, ex: 't-client-01' or 't-client-01.domain.edu'
+        Path to text file containing one hostname per line, ex: 'D:\computers.txt'
+        First section of a hostname to generate a list, ex: g-labpc- will create a list of all hostnames that start with 
+        g-labpc- (g-labpc-01. g-labpc-02, g-labpc-03..).
+
+    .PARAMETER SendPings
+        Switch parameter to test target machines for connectivity before performing operations.
+
+    .EXAMPLE
+        send-files -ComputerName test-client -sourcepath C:\test.txt -destinationpath C:\ -SendPings
+    #>
+    param (
+        [Parameter(
+            Mandatory = $true
+        )]
+        $ComputerName,
+        [ValidateScript({
+                if (Test-Path $_ -ErrorAction SilentlyContinue) {
+                    return $true
+                }
+                else {
+                    Write-Error "SourcePath does not exist. Please enter a valid path."
+                    return $false                
+                }
+            })]
+        [string]$sourcepath,
+        [string]$destinationpath,
+        [switch]$SendPings
+    )
+
+    $ComputerName = GetTargets -TargetComputer $ComputerName
+
+    ## Ping Test for Connectivity:
+    if ($SendPings) {
+        $ComputerName = TestConnectivity -ComputerName $ComputerName
+    }
+
+    # create full output filepath for log file (shows confirmation that files were copied to each remote computer)
+    $output_filepath = "SendFiles-$(get-date -format 'yyyy-MM-dd').txt"
+
+    $informational_string = ""
+
+    ForEach ($single_computer in $ComputerName) {
+        if ($single_computer) {
+            $file_copied = $false
+            if ([System.IO.Directory]::Exists("\\$single_computer\c$")) {
+                Invoke-Command -ComputerName $single_computer -Scriptblock {
+
+                    $destination_folder = $using:destinationpath
+                    if (-not (Test-Path $destination_folder -ErrorAction SilentlyContinue)) {
+                        Write-Host "Creating $destination_folder directory on $env:COMPUTERNAME."
+                        New-Item -Path $destination_folder -ItemType Directory -Force | Out-Null
+                    }
+                }
+
+                $target_session = New-PSSession $single_computer
+
+                Copy-Item -Path "$sourcepath" -Destination "$destinationpath" -ToSession $target_session -Recurse
+                $informational_string += "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Transfer of $sourcepath to $destinationpath ($single_computer) complete.`n"
+
+                $file_copied = $true
+
+
+                Remove-PSSession $target_session
+                            
+            }
+            
+            if (-not $file_copied) {
+                $informational_string += "[$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')] :: Failed to copy $sourcepath to $destinationpath on $single_computer.`n"
+            }
+            
+        }
+    }
+        
+    ## Append text to file here:
+    $informational_string | Out-File -FilePath $output_filepath -Append
+    # $ComputerName | Out-File -FilePath $output_filepath -Append
+    "`nThe Scan-ForApporFilepath function can be used to verify file/folders' existence on target computers." | Out-File -FilePath $output_filepath -Append -Force
+        
+    Invoke-Item "$output_filepath"        
+}
+
 Export-ModuleMember -Function *-*
--- README.md
+++ README.md
@@ -6,6 +6,39 @@ The reports module from: https://github.com/albddnbn/PSTechSupportMenu.
 
 More information: https://github.com/albddnbn/PSTechReports/wiki
 
+## Functions available include:
+
+- Get-AssetInformation
+- Get-ComputerDetails
+- Get-ConnectedPrinters
+- Get-CurrentUser
+- Get-InstalledDotNetversions
+- Get-IntuneHardwareIDs
+- Get-InventoryDetails
+- Get-TempProfiles
+- Ping-TestReport
+- Scan-ForAppOrFilePath
+- Scan-SoftwareInventory
+- Test-ConnectivityQuick
+
+## How GetTargets.ps1 works:
+
+The **ComputerName** parameter will take a few things as input.
+1. Single computer name
+2. Comma-separated list of computer names
+3. A substring of a computer name
+
+OR
+
+4. A path to text file containing list of computer names
+
+**A regex pattern is used to validate computer name input for all values except text file.**
+
+If you specify a "single computer name": s-client-1
+
+
+
+
 ### Get-IntuneHardwareIDs:
 
 This function uses the script: https://www.powershellgallery.com/packages/Get-WindowsAutopilotInfo/3.8 to collect hardware IDs.
